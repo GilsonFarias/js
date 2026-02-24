f826a60d3cfcf8da8a0a04f5b97cf3aa83741737
Gilson <gilsonfarias.dev@gmail.com>
2026-02-23 22:50:47 -0400

    fix(usuario) altera documentação

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -2,23 +2,26 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, NotFoundEx
 import { UsuariosService } from './usuarios.service';
 import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { UpdateUsuarioDto } from './dto/update-usuario.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('usuarios')
 @Controller('usuarios')
 export class UsuariosController {
   constructor(private readonly usuariosService: UsuariosService) {}
 
+  @ApiOperation({ summary: 'Criar usuário' })
   @Post()
   create(@Body() createUsuarioDto: CreateUsuarioDto) {
     return this.usuariosService.create(createUsuarioDto);
   }
 
+  @ApiOperation({ summary: 'Lista todos os usuário' })
   @Get()
   findAll() {
     return this.usuariosService.findAll();
   }
 
+  @ApiOperation({ summary: 'Consulta usuário por id' })
   @Get(':id')
   async findOne(@Param('id') id: string) {
     const usuario = await this.usuariosService.findOne(+id);
@@ -26,12 +29,14 @@ export class UsuariosController {
     return usuario;
   }
 
+  @ApiOperation({ summary: 'Altera usuário' })
   @Patch(':id')
   async update(@Param('id') id: string, @Body() updateUsuarioDto: UpdateUsuarioDto) {
     const usuario = await this.usuariosService.update(+id, updateUsuarioDto);
     if(!usuario) throw new NotFoundException();
   }
 
+  @ApiOperation({ summary: 'Deleta usuário' })
   @Delete(':id')
   @HttpCode(204)
   async remove(@Param('id') id: string) {
